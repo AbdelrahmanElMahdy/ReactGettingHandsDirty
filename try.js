0f44e41b1b6036bc50b87621fdbf2e1f92506770
MHDI3 <a.b.mahdey@gmail.com>
2021-01-24 18:33:42 +0200

    adding files

---
+++ try.js
@@ -0,0 +1,29 @@
+export function lol(name){
+        return name;
+}
+
+
+// const _unite=new WeakMap();
+// const _move=new WeakMap();
+// export class Shape {
+//         constructor(unite){
+//           _unite.set(this,unite);
+//           _move.set(this, () => {
+//                      console.log('move');
+//                 });
+//                 }
+
+//         get unite(){
+//                 return _unite.get(this);
+//         }
+//         set unite(value){
+//                 _unite.set(this,value);
+//         };
+//         draw(){
+//                 _move.get(this)()
+                
+//                 console.log(_unite.get(this));
+//         }
+
+// }
+
---
+++ try2.js
@@ -0,0 +1,4 @@
+import { lol } from "./try.js";
+console.log( lol(7) );
+
+let c=lol(5)
